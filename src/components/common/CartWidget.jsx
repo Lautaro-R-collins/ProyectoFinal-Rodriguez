545925530ef7385d8ab6a9d71a973150d32daf76
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-09-26 16:54:48 -0300

    commit

--- src/components/common/CartWidget.jsx
+++ src/components/common/CartWidget.jsx
@@ -1,42 +1,19 @@
 import { FaShoppingCart } from "react-icons/fa";
-import useCart from "../../context/useCart"; 
-import { Link } from "react-router-dom";
+import useCart from "../../context/useCart";
 
-const CartWidget = () => {
+const CartWidget = ({ onOpen }) => {
   const { totalQuantity } = useCart();
 
   return (
-    <Link to="/cart" style={styles.link}>
-      <div style={styles.container}>
-        <FaShoppingCart size={22} />
-        {totalQuantity > 0 && (
-          <span style={styles.badge}>{totalQuantity}</span>
-        )}
-      </div>
-    </Link>
+    <div onClick={onOpen} className="relative cursor-pointer">
+      <FaShoppingCart size={22} />
+      {totalQuantity > 0 && (
+        <span className="absolute -top-3 -right-3 bg-[#E6CA4D] text-black font-bold rounded-full px-1.5 py-0.5 text-xs">
+          {totalQuantity}
+        </span>
+      )}
+    </div>
   );
-};
-
-const styles = {
-  link: {
-    textDecoration: "none",
-    color: "inherit",
-  },
-  container: {
-    position: "relative",
-    display: "inline-block",
-    cursor: "pointer",
-  },
-  badge: {
-    position: "absolute",
-    top: "-12px",
-    right: "-12px",
-    backgroundColor: "#FA8072",
-    color: "white",
-    borderRadius: "50%",
-    padding: "1px 6px",
-    fontSize: "12px",
-  },
 };
 
 export default CartWidget;
