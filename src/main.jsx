edfb21b881bb1246452c8f79988531455a18bb83
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-09-29 17:58:26 -0300

    main actualizado

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
+import { FavoritesProvider } from "./context/FavoritesContext.jsx";
 import Footer from "./components/common/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <CartProvider>
-    <App />
-    <Footer />
+    <FavoritesProvider>
+      <App />
+      <Footer />
+    </FavoritesProvider>
   </CartProvider>
 );
