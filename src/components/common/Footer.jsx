6a8f6bbb38a10a981fbb5d82dd20a3d89851c33c
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-09-29 17:59:33 -0300

    identacion

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,9 @@
-import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
-import Logo from "../../assets/Logo/logoUrban.png"; // reemplaza con tu logo
+import Logo from "../../assets/Logo/logoUrban.png";
 
 const Footer = () => {
   return (
     <footer className="bg-black text-white py-8">
       <div className="container mx-auto px-4 flex flex-col sm:flex-row items-center justify-between gap-6">
-        
         {/* Logo */}
         <div className="flex items-center justify-center sm:justify-start">
           <img src={Logo} alt="Logo" className="h-10 w-auto" />
@@ -13,7 +11,8 @@ const Footer = () => {
 
         {/* Derechos */}
         <p className="text-center sm:text-right text-sm text-gray-400">
-          © {new Date().getFullYear()} Urban Style. Todos los derechos reservados.
+          © {new Date().getFullYear()} Urban Style. Todos los derechos
+          reservados.
         </p>
       </div>
     </footer>
